1c75259c7e70613f69fd98706a56817305880eea
Geraldinne-Jaramillo <geraldinne8821@gmail.com>
2025-07-21 18:44:19 -0500

    feat buscar un evento por palabra clave

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,5 +40,44 @@ public class Main {
         for (Evento e : eventos) {
             System.out.println(e);
         }
+
+        //mi parte
+        boolean buscarMas = true;
+        while (buscarMas) {
+            System.out.println("\n--- Buscar eventos por palabra clave ---");
+            System.out.println("Introduce la palabra clave para buscar eventos:");
+            String palabraClave = scanner.nextLine().trim().toLowerCase(); //.trim() es para quitar los espacios en blanco
+
+            List<Evento> resultadosBusqueda = new ArrayList<>();
+            for (Evento evento : eventos) {
+                if (evento.getNombre().toLowerCase().contains(palabraClave)) { //compara la palabra ccon el evento guardado
+                    resultadosBusqueda.add(evento);
+                }
+            }
+
+            System.out.println("\n--- Resultados de la búsqueda ---");
+            if (resultadosBusqueda.isEmpty()) {
+                System.out.println("No se encontraron eventos con la palabra clave: '" + palabraClave + "'");
+                System.out.println("¿Quieres intentar con otra palabra clave? (sí/no):");
+                String respuestaBusqueda = scanner.nextLine().trim().toLowerCase();
+                if (!respuestaBusqueda.equals("sí") && !respuestaBusqueda.equals("si")) {
+                    buscarMas = false;
+                    System.out.println("Gracias por tu ingreso");// Salir del bucle de búsqueda
+                }
+            } else {
+                System.out.println("Eventos encontrados con la palabra clave: '" + palabraClave + "'");
+                for (Evento resultado : resultadosBusqueda) {
+                    System.out.println(resultado);
+                }
+                // Si se encontraron resultados, preguntar si quiere buscar más
+                System.out.println("¿Quieres realizar otra búsqueda? (sí/no):");
+                String respuestaBusqueda = scanner.nextLine().trim().toLowerCase();
+                if (!respuestaBusqueda.equals("sí") && !respuestaBusqueda.equals("si")) {
+                    buscarMas = false; // Salir del bucle de búsqueda
+                    System.out.println("Gracias por tu ingreso");
+                }
+            }
+        }
+
     }
 }
